8de88eb50037be972cf6763c43a6f53a6fdb63fe
Setiyo Adi Winoto <adiwinotos@gmail.com>
2025-12-24 05:48:09 +0700

    edit grid layout

--- basics/06_grid_layout.py
+++ basics/06_grid_layout.py
@@ -4,4 +4,22 @@ root = tk.Tk()
 root.title("Lesson 6 - Grid layout")
 root.geometry("400x300")
 
-nama_label = tk.Label()
+nama_label = tk.Label(root, text="Nama")
+nama_label.grid(row=0, column=0, sticky="w")
+
+nama_entry = tk.Entry(root)
+nama_entry.grid(row=0, column=2)
+
+nama_button = tk.Button(root, text="Check")
+nama_button.grid(row=0, column=3, rowspan=2)
+
+kode_label = tk.Label(root, text="Kode")
+kode_label.grid(row=1, column=0, sticky="w")
+
+kode_entry = tk.Entry(root)
+kode_entry.grid(row=1, column=2)
+
+#kode_button = tk.Button(root, text="Check")
+#kode_button.grid(row=1, column=3)
+
+root.mainloop()
