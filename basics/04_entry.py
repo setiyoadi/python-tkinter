c2c593c4c4630931649bd9846358670ec2131318
Setiyo Adiwinoto <adiwinotos@gmail.com>
2025-12-23 15:35:56 +0700

    add command to button and print output to tty and main window

--- basics/04_entry.py
+++ basics/04_entry.py
@@ -4,10 +4,28 @@ root = tk.Tk()
 root.geometry("400x300")
 root.title("Lesson 3 - Entry")
 
+def print_to_tty():
+    ambil_nama = nama_entry.get()
+    print(f"Halo, {ambil_nama}")
+
+def print_to_scr():
+    ambil_nama = nama_entry.get()
+    output_label.configure(text=f"Halo, {ambil_nama}")
+    
+
 nama_label = tk.Label(root, text="Nama:")
-nama_label.pack(anchor="w", padx=20)
+nama_label.pack(anchor="w", padx=20, pady=10)
 
 nama_entry = tk.Entry(root)
-nama_entry.pack(fill="x", padx=20)
+nama_entry.pack(fill="x", padx=20, pady=10)
+
+tty_button = tk.Button(root, text="Print to tty", command=print_to_tty)
+tty_button.pack(anchor="w", padx=20, pady=10)
+
+scr_button = tk.Button(root, text="Print to screen", command=print_to_scr)
+scr_button.pack(anchor="w", padx=20, pady=10)
+
+output_label = tk.Label(root, text="")
+output_label.pack(anchor="w", padx=20, pady=10)
 
 root.mainloop()
