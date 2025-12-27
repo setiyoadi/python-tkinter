a5efb7f15223c5503d88257ec655fecc7e1da6ad
Setiyo Adi Winoto <adiwinotos@gmail.com>
2025-12-27 19:55:23 +0700

    tidy up UI, auto clear the entry

--- basics/08_stringvar.py
+++ basics/08_stringvar.py
@@ -12,38 +12,40 @@ guestname = tk.StringVar()
 def greet_user():
     name = guestname.get()
     greeting.config(text=f"Hello, {name}")
+    guestname.set("")
+    name_entry.focus()
 
 #make entry frame on top
-entry_frm = tk.Frame(root, bg="red")
+entry_frm = tk.Frame(root)
 entry_frm.pack(side="top", fill="x", expand=True)
 
 #make label frame to display label in the middle, filling the space
-lbl_frm = tk.Frame(root,bg="yellow")
+lbl_frm = tk.Frame(root)
 lbl_frm.pack(side="top", fill="both", expand=True)
 
 #make button frame at the bottom
-bttn_frm = tk.Frame(root, bg="green")
+bttn_frm = tk.Frame(root)
 bttn_frm.pack(side="bottom", fill="x", expand=True)
 
 #add label in the entry_frm
 name_label = tk.Label(entry_frm, text="Name")
-name_label.grid(row=0, column=0)
+name_label.grid(row=0, column=0, padx=5, pady=5)
 
 #add entry in the entry_frm
 name_entry = tk.Entry(entry_frm, textvariable=guestname)
-name_entry.grid(row=0, column=1)
+name_entry.grid(row=0, column=1, padx=5, pady=5)
 
 #add label preview
 prev_lbl = tk.Label(lbl_frm, textvariable=guestname)
-prev_lbl.pack()
+prev_lbl.pack(padx=5, pady=5)
 
 #add label to greet the user
 greeting = tk.Label(lbl_frm, text="")
-greeting.pack()
+greeting.pack(padx=5, pady=5)
 
 #add button to print greeting
 greet_bttn = tk.Button(bttn_frm, text="Print greeting", command=greet_user)
-greet_bttn.pack()
+greet_bttn.pack(padx=5, pady=5)
 
 
 
