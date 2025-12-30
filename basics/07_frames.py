7c199c44b9b83a5310da8917338ca7def62f4000
Setiyo Adi Winoto <adiwinotos@gmail.com>
2025-12-31 05:17:23 +0700

    add escape event, disable text info editing

--- basics/07_frames.py
+++ basics/07_frames.py
@@ -1,14 +1,23 @@
 import tkinter as tk
 
+def keluar_apps(event=None):
+    root.destroy()
+
 root = tk.Tk()
 root.title("Lesson 7 - Frames")
 root.geometry("600x300")
+
+top_frm = tk.Frame(root, bg="yellow")
+top_frm.pack(side="top", fill="x")
 
 left_frm = tk.Frame(root, bg="green")
 left_frm.pack(side="left", fill="both", expand=True)
 
 right_frm = tk.LabelFrame(root, text="Informasi")
 right_frm.pack(side="right")
+
+judul = tk.Label(top_frm, text="BELAJAR MENGATUR FRAME")
+judul.pack()
 
 nama_lbl = tk.Label(left_frm, text="Nama")
 nama_lbl.grid(row=0, column=0, padx=5, pady=5)
@@ -26,7 +35,8 @@ info_text = tk.Text(right_frm, width=30, height=10, wrap="word")
 info_text.pack()
 
 info_text.insert("1.0", "Text percobaan jadi kita coba saja masukan apa saja teks kedalam kotak sehingga semua akan menjadi baik baik saja")
+info_text.config(state="disabled")
 
-
+root.bind("<Escape>", keluar_apps)
 
 root.mainloop()
